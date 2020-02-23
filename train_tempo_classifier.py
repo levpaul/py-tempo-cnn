bb9640302a0401d6923aa1be5fe51b2f9467164d
Levi Lovelock <lovelock.levi@gmail.com>
2020-02-23 15:11:08 -0800

    Update SSA model training code

--- train_tempo_classifier.py
+++ train_tempo_classifier.py
@@ -21,25 +21,153 @@ class PaperNet(nn.Module):
         # Short Conv NN x3
 
         # Spectros come in at 40 x 256
-        self.conv1 = nn.Conv2d(1, 16, 5) # 16x36x252
-        self.conv1_bn = nn.BatchNorm2d(16*36*252)
-        self.conv2 = nn.Conv2d(16, 16, 5) # 16x32x248
-        self.conv2_bn = nn.BatchNorm2d(16*36*252)
-        self.conv3 = nn.Conv2d(16, 16, 5) # 16x28x244
-        self.conv3_bn = nn.BatchNorm2d(16*36*252)
+        # ====== SHORT FILTERS ======
+        self.sf_conv1 = nn.Conv2d(1, 16, (1,5)) # 16x36x252
+        self.sf_conv1_bn = nn.BatchNorm2d(1)
+        self.sf_conv2 = nn.Conv2d(16, 16, (1,5)) # 16x32x248
+        self.sf_conv2_bn = nn.BatchNorm2d(16)
+        self.sf_conv3 = nn.Conv2d(16, 16, (1,5)) # 16x28x244
+        self.sf_conv3_bn = nn.BatchNorm2d(16)
 
         # Multi filter NN x4
-        # Dense Layers
+        # ====== MULTI FILTER MODS ======
+        m=5
+        input_chans=16
+        self.mf1_ap = nn.AvgPool2d((m,1))
+        self.mf1_bn = nn.BatchNorm2d(input_chans)
+        self.mf1_conv1 = nn.Conv2d(input_chans,24,(1,32))
+        self.mf1_conv2 = nn.Conv2d(input_chans,24,(1,64))
+        self.mf1_conv3 = nn.Conv2d(input_chans,24,(1,96))
+        self.mf1_conv4 = nn.Conv2d(input_chans,24,(1,128))
+        self.mf1_conv5 = nn.Conv2d(input_chans,24,(1,192))
+        self.mf1_conv6 = nn.Conv2d(input_chans,24,(1,244)) # This is a single fullscale, cutoff by previous CNNs
+        self.mf1_conv_final = nn.Conv2d(24,36,1)
 
-        # self.fc1 = nn.Linear(14*122, 50) # 120
-        # self.fc2 = nn.Linear(120, 50) # 50
-        # self.fc3 = nn.Linear(50, 256) # 256 final out
+        m=2
+        input_chans=36
+        self.mf2_ap = nn.AvgPool2d((m,1))
+        self.mf2_bn = nn.BatchNorm2d(input_chans)
+        self.mf2_conv1 = nn.Conv2d(input_chans,24,(1,32))
+        self.mf2_conv2 = nn.Conv2d(input_chans,24,(1,64))
+        self.mf2_conv3 = nn.Conv2d(input_chans,24,(1,96))
+        self.mf2_conv4 = nn.Conv2d(input_chans,24,(1,128))
+        self.mf2_conv5 = nn.Conv2d(input_chans,24,(1,192))
+        self.mf2_conv6 = nn.Conv2d(input_chans,24,(1,244)) # This is a single fullscale, cutoff by previous CNNs
+        self.mf2_conv_final = nn.Conv2d(24,36,1)
+
+        self.mf3_ap = nn.AvgPool2d((m,1))
+        self.mf3_bn = nn.BatchNorm2d(input_chans)
+        self.mf3_conv1 = nn.Conv2d(input_chans,24,(1,32))
+        self.mf3_conv2 = nn.Conv2d(input_chans,24,(1,64))
+        self.mf3_conv3 = nn.Conv2d(input_chans,24,(1,96))
+        self.mf3_conv4 = nn.Conv2d(input_chans,24,(1,128))
+        self.mf3_conv5 = nn.Conv2d(input_chans,24,(1,192))
+        self.mf3_conv6 = nn.Conv2d(input_chans,24,(1,244)) # This is a single fullscale, cutoff by previous CNNs
+        self.mf3_conv_final = nn.Conv2d(24,36,1)
+
+        # GPU NOT ENOUGH RAM
+        # self.mf4_ap = nn.AvgPool2d((m,1))
+        # self.mf4_bn = nn.BatchNorm2d(input_chans)
+        # self.mf4_conv1 = nn.Conv2d(input_chans,24,(1,32))
+        # self.mf4_conv2 = nn.Conv2d(input_chans,24,(1,64))
+        # self.mf4_conv3 = nn.Conv2d(input_chans,24,(1,96))
+        # self.mf4_conv4 = nn.Conv2d(input_chans,24,(1,128))
+        # self.mf4_conv5 = nn.Conv2d(input_chans,24,(1,192))
+        # self.mf4_conv6 = nn.Conv2d(input_chans,24,(1,244)) # This is a single fullscale, cutoff by previous CNNs
+        # self.mf4_conv_final = nn.Conv2d(24,36,1)
+
+        # ====== DENSE LAYERS ======
+        # Dense Layers
+        # mfmod_size = 36*40908
+        mfmod_size = 205632
+        self.dl_bn1 = nn.BatchNorm1d(mfmod_size)
+        self.dl_do = nn.Dropout(0.5)
+        self.dl_fc1 = nn.Linear(mfmod_size, 64)
+        self.dl_bn2 = nn.BatchNorm1d(64)
+        self.dl_fc2 = nn.Linear(64,64)
+        self.dl_bn3 = nn.BatchNorm1d(64)
+        self.dl_fc3 = nn.Linear(64,256)
+
 
     def forward(self, x):
-        x = F.elu(self.conv1(x))
-        x = F.elu(self.conv2_bn(self.conv2(x)))
-        x = F.elu(self.conv3_bn(self.conv3(x)))
-        return F.softmax(x,dim=1)
+        # ====== SHORT FILTERS ======
+        x = F.elu(self.sf_conv1(self.sf_conv1_bn(x)))
+        x = F.elu(self.sf_conv2(self.sf_conv2_bn(x)))
+        x = F.elu(self.sf_conv3(self.sf_conv3_bn(x)))
+
+        # ====== MULTI FILTER MODS ======
+        x = self.mf1_ap(x)
+        x = self.mf1_bn(x)
+        c1 = self.mf1_conv1(x)
+        c2 = self.mf1_conv2(x)
+        c3 = self.mf1_conv3(x)
+        c4 = self.mf1_conv4(x)
+        c5 = self.mf1_conv5(x)
+        c6 = self.mf1_conv6(x)
+        x = torch.cat((c1,c2,c3,c4,c5,c6), dim=3)
+        x = self.mf1_conv_final(x)
+
+        # SPEED UP BEGINNING TRAINGIN
+        # x = self.mf2_ap(x)
+        # x = self.mf2_bn(x)
+        # c1 = self.mf2_conv1(x)
+        # c2 = self.mf2_conv2(x)
+        # c3 = self.mf2_conv3(x)
+        # c4 = self.mf2_conv4(x)
+        # c5 = self.mf2_conv5(x)
+        # c6 = self.mf2_conv6(x)
+        # x = torch.cat((c1,c2,c3,c4,c5,c6), dim=3)
+        # x = self.mf2_conv_final(x)
+        #
+        # x = self.mf3_ap(x)
+        # x = self.mf3_bn(x)
+        # c1 = self.mf3_conv1(x)
+        # c2 = self.mf3_conv2(x)
+        # c3 = self.mf3_conv3(x)
+        # c4 = self.mf3_conv4(x)
+        # c5 = self.mf3_conv5(x)
+        # c6 = self.mf3_conv6(x)
+        # x = torch.cat((c1,c2,c3,c4,c5,c6), dim=3)
+        # x = self.mf3_conv_final(x)
+
+        # GPU not enough RAM
+        # x = self.mf4_ap(x)
+        # x = self.mf4_bn(x)
+        # c1 = self.mf4_conv1(x)
+        # c2 = self.mf4_conv2(x)
+        # c3 = self.mf4_conv3(x)
+        # c4 = self.mf4_conv4(x)
+        # c5 = self.mf4_conv5(x)
+        # c6 = self.mf4_conv6(x)
+        # x = torch.cat((c1,c2,c3,c4,c5,c6), dim=3)
+        # x = self.mf4_conv_final(x)
+
+        # ====== DENSE LAYERS ======
+        x = torch.flatten(x,1)
+        x = self.dl_do(x)
+        x = self.dl_bn1(x)
+        x = F.elu(self.dl_fc1(x))
+        x = self.dl_bn2(x)
+        x = F.elu(self.dl_fc2(x))
+        x = self.dl_bn3(x)
+        x = self.dl_fc3(x)
+
+        output = F.softmax(x,dim=1)
+        # print('output', output.shape)
+        return output
+
+
+class MFMod(object):
+    def __init__(self,m, input_chans):
+        self.ap = nn.AvgPool2d((m,1))
+        self.bn = nn.BatchNorm2d(input_chans)
+        self.conv1 = nn.Conv2d(input_chans,24,(1,32))
+        self.conv2 = nn.Conv2d(input_chans,24,(1,64))
+        self.conv3 = nn.Conv2d(input_chans,24,(1,96))
+        self.conv4 = nn.Conv2d(input_chans,24,(1,128))
+        self.conv5 = nn.Conv2d(input_chans,24,(1,192))
+        self.conv6 = nn.Conv2d(input_chans,24,(1,244)) # This is a single fullscale, cutoff by previous CNNs
+        self.conv_final = nn.Conv2d(24,36,1)
 
 class BadNet(nn.Module):
     def __init__(self):
@@ -98,7 +226,11 @@ def train(net, loader, device, opt):
 
         outs = net(data)
         # loss = loss_func(outs, targets)
-        loss = F.nll_loss(outs, targets)
+        # print('outs', outs, outs.shape)
+        # print('targs', targets)
+
+        # loss = F.nll_loss(outs, targets)
+        loss = F.cross_entropy(outs, targets)
         loss.backward() # <- compute backprop derivs
         opt.step()
 
@@ -136,9 +268,9 @@ def run():
 
     print('Initializing Data Loaders...')
     tr_loader = DataLoader(FMASpectrogramsDataset(train=True),
-                           batch_size=bsize, shuffle=True, num_workers=1, pin_memory=True)
+                           batch_size=bsize, shuffle=True, num_workers=10, pin_memory=True)
     te_loader = DataLoader(FMASpectrogramsDataset(train=False),
-                           batch_size=bsize, shuffle=True, num_workers=1, pin_memory=True)
+                           batch_size=bsize, shuffle=True, num_workers=10, pin_memory=True)
 
     print('Initializing network and optimizer')
     if args.network == 'paper':
@@ -151,7 +283,9 @@ def run():
     if cuda:
         net = net.cuda().half()
 
-    opt = optim.SGD(net.parameters(), lr=0.01)
+    # opt = optim.SGD(net.parameters(), lr=0.01)
+    # default eps causes NaNs for 16bit training
+    opt = optim.Adam(net.parameters(), lr=0.01, eps=1e-4)
 
     for e in range(1,epochs+1):
         print('Begining training at epoch {:d}'.format(e))
