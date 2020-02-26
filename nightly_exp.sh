cfd031d097ac7b7a7a4837c65d17330abfc78e15
Levi Lovelock <lovelock.levi@gmail.com>
2020-02-25 19:17:06 -0800

    update nightly

--- nightly_exp.sh
+++ nightly_exp.sh
@@ -7,8 +7,8 @@ trap 'kill $(jobs -p)' EXIT
 mkdir -p logs
 timestamp=$(date '+%m%d-%H%M%S')
 
-python -u ./train_tempo_classifier.py -r nn-bad-679.pt > "logs/${timestamp}-${n}.out" &
-python -u ./train_tempo_classifier.py -r nn-ssa-131.pt > "logs/${timestamp}-${n}.out" &
+python -u ./train_tempo_classifier.py -r nn-bad-679.pt > "logs/${timestamp}-bad.out" &
+python -u ./train_tempo_classifier.py -r nn-ssa-131.pt > "logs/${timestamp}-ssa.out" &
 
 #networks=(ssa bad)
 #for n in "${networks[@]}"; do
