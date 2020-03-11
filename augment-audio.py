e95a23ebac563e2a0ec21e4b5319285adf8c68b4
Levi Lovelock <lovelock.levi@gmail.com>
2020-03-11 00:06:58 -0700

    Update augment script

--- augment-audio.py
+++ augment-audio.py
@@ -1,183 +1,48 @@
 #!/usr/bin/env python
 
 
-import sox
 import string
 import random
 import argparse
 
-from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing.dummy import Pool as ThreadPoolpool
+from multiprocessing import cpu_count
 
 # ========================================================
 #   Arg Parsing
 # ========================================================
 def parse_args():
-    parser = argparse.ArgumentParser(description='Data augmentation script - pass in a directory of ')
+    parser = argparse.ArgumentParser(description='Data augmentation script - pass in a directory which contains a test or train (or both) directories containing audio (wav) files.')
     parser.add_argument('-n', '--end-dataset-size', type=int, default=100000, help='Specify the total number of files to generate')
     parser.add_argument('-s', '--source-dir', required=True, help='The directory the train and or test directories are in')
     parser.add_argument('-o', '--output-dir', required=True, help='The directory to use for outputting augmented data into')
+    parser.add_argument('-t', '--augment-type', choices=['train', 'test', 'both'], default='both', help='The type of datasets to compile')
+    parser.add_argument('-j', '--number-workers', type=int, default=cpu_count(), help='The number of workers to use for parallel processing')
     return parser.parse_args()
 
 # ========================================================
 # No diff between poolings except normal Pool gives Exceptions at runtime
-pool = ThreadPool(50)
 
-class DataAugmenter:
-    def __init__(self, file_names, n_times):
-        self.file_names =  file_names
-        self.n_times = n_times
-        self.idx = 0
+def augment_data(source_dir, out_dir, train_set, test_set, num_workers):
+    print('To implement')
 
-    def __iter__(self):
-        return self
+def gen_spectrograms(source_dir, out_dir, train_set, test_set, num_workers):
+    print('To implement')
 
-    def __next__(self):
-        if self.idx < self.n_times:
-            result = self.file_names[self.idx % len(self.file_names)]
-            self.idx += 1
-            return result
-        raise StopIteration
-
-
-file_names = [
-    # '100-44-straight-chords2.wav',
-    # '100-44-straight-fingers.wav',
-    # '100-44-straight-notes.wav',
-    # '100-44-swing-chords.wav',
-    # '100-44-swing-fingers.wav',
-    # '100-44-swing-solo.wav',
-    'test-100-2.wav',
-    'test-100.wav',
-]
-
-
-effect_probs = {
-    'chorus': 0.2,
-    'compression': 0.35,
-    'delay': 0.4,
-    'flanger': 0.4,
-    'highpass': 0.1,
-    'lowpass': 0.1,
-    'overdrive': 0.35,
-    'phaser': 0.3,
-    'reverb': 0.45,
-    'tremolo': 0.25,
-}
-# ============================================================
-#      EFFECTS
-# ============================================================
-
-def chorus_effect(tfm):
-    voices = random.randint(2,5)
-    chr_shapes = ['s' if random.random() < 0.5 else 't' for _ in range(voices)]
-    chr_delays = [random.randint(20,100) for _ in range(voices)]
-    chr_speeds = [random.randint(10,499)/100. for _ in range(voices)]
-    tfm.chorus(n_voices=voices, shapes=chr_shapes, delays=chr_delays, speeds=chr_speeds)
-
-def compression_effect(tfm):
-    tfm.contrast(amount=random.randint(50,100))
-
-def delay_effect(tfm):
-    echos = random.randint(1,5)
-    dl_delays = [random.randint(14,800) for _ in range(echos)]
-    dl_decays = [random.randint(20,60)/100. for _ in range(echos)]
-    tfm.echo(n_echos=echos, delays=dl_delays, decays=dl_decays)
-
-def flanger_effect(tfm):
-    fl_depth = random.randint(2,10)
-    fl_speed = random.randint(10,1000)/100.
-    fl_shape = 'sine' if random.random() < 0.5 else 'triangle'
-    tfm.flanger(depth=fl_depth, speed=fl_speed, shape=fl_shape)
-
-def highpass_effect(tfm):
-    tfm.highpass(frequency=random.randint(30,500))
-
-def lowpass_effect(tfm):
-    tfm.lowpass(frequency=random.randint(3000,10000))
-
-def overdrive_effect(tfm):
-    od_gain = random.randint(5,30)
-    od_color = random.randint(10,30)
-    tfm.overdrive(gain_db=od_gain, colour=od_color)
-    tfm.gain(limiter=True)
-
-def phaser_effect(tfm):
-    ph_delay = random.randint(1,5)
-    ph_decay = random.randint(10,50)/100.
-    ph_speed = random.randint(10,200)/100.
-    ph_shape = 'sinusoidal' if random.random() < 0.5 else 'triangular'
-    tfm.phaser(delay=ph_delay, decay=ph_decay, speed=ph_speed, modulation_shape=ph_shape)
-
-def reverb_effect(tfm):
-    rv_reverb = random.randint(5,70)
-    rv_pre_delay = random.randint(0,20)
-    tfm.reverb(reverberance=rv_reverb, pre_delay=rv_pre_delay)
-
-def tremolo_effect(tfm):
-    tr_speed = random.randint(1,30)
-    tr_depth = random.randint(10,60)
-    tfm.tremolo(speed=tr_speed, depth=tr_depth)
-
-# ============================================================
-
-def apply_effect(tfm, effect_func, chance, file_meta):
-    if random.random() < chance:
-        file_meta['name'] += '-'+effect_func.__name__[:2]
-        effect_func(tfm)
-        tfm.gain(normalize=True)
-
-# Files from DB
-def transform(f):
-    tfm = sox.Transformer()
-
-    pitch_change = random.randint(-12,12)
-    tfm.pitch(n_semitones=pitch_change)
-    tfm.gain(normalize=True)
-
-    old_file_short = '-'.join(f[:-4].split('-')[2:4])
-    rand_seq = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-    file_meta = {'name': '{:s}-{:s}-pi{:+d}'.format(rand_seq, old_file_short,pitch_change)}
-
-    apply_effect(tfm, compression_effect, effect_probs['compression'], file_meta)
-    apply_effect(tfm, chorus_effect, effect_probs['chorus'], file_meta)
-    apply_effect(tfm, delay_effect, effect_probs['delay'], file_meta)
-    apply_effect(tfm, flanger_effect, effect_probs['flanger'], file_meta)
-    apply_effect(tfm, highpass_effect, effect_probs['highpass'], file_meta)
-    apply_effect(tfm, lowpass_effect, effect_probs['lowpass'], file_meta)
-    apply_effect(tfm, overdrive_effect, effect_probs['overdrive'], file_meta)
-    apply_effect(tfm, phaser_effect, effect_probs['phaser'], file_meta)
-    apply_effect(tfm, reverb_effect, effect_probs['reverb'], file_meta)
-    apply_effect(tfm, tremolo_effect, effect_probs['tremolo'], file_meta)
-    tfm.lowpass(5500)
-
-    orig_tempo = 100.
-    new_tempo = random.randint(50,150)
-    tempo_factor = new_tempo/orig_tempo
-    if abs(tempo_factor - 1.0) <= 0.1:
-        tfm.stretch(tempo_factor)
-    else:
-        tfm.tempo(tempo_factor, audio_type='m')
-
-    tfm.gain(normalize=True, limiter=True)
-    file_meta['name'] += '-{:d}bpm'.format(new_tempo)
-
-    tfm.build('raw/data/'+f, '/ssd/augout-test/{}.mp3'.format(file_meta['name']))
+def squash_to_dataset(source_dir, out_dir, train_set, test_set, num_workers):
+    print('To implement')
 
 
 def run():
     args = parse_args()
-    print("Args:", args)
+    train_set = True if args.augment_type == 'both' or args.augment_type == 'train' else False
+    test_set = True if args.augment_type == 'both' or args.augment_type == 'test' else False
+
+    augment_data(source_dir=args.source_dir, out_dir=args.output_dir, train_set=train_set, test_set=test_set, num_workers=args.number_workers)
+    gen_spectrograms(source_dir=args.source_dir, out_dir=args.output_dir, train_set=train_set, test_set=test_set, num_workers=args.number_workers)
+    squash_to_dataset(source_dir=args.source_dir, out_dir=args.output_dir, train_set=train_set, test_set=test_set, num_workers=args.number_workers)
     # augmenter = DataAugmenter(file_names, args.end_dataset_size)
     # pool.map(transform, augmenter)
 
 if __name__ == '__main__':
     run()
-
-
-
-# Sox Commands I Like:
-
-# chorus gain-in gain-out delay decay speed depth [ -s | -t ] (sin/triangle)
-# play classic.wav chorus 1 1 55 .4 .25 10 -t
-
-# Tempo
